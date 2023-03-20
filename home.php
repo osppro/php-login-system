42acd762e177a1230b858cd1ac7e9b79cbfc15be
osppro <39586345+osppro@users.noreply.github.com>
2023-03-20 13:11:53 +0300

    Update home.php

---
+++ home.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Login form</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<h4></h4>
+				</div>
+				<div class="col-md-4">
+					<h2 class="well text-center">Login Form</h2>
+				</div>
+				<div class="col-md-4">
+					<h4></h4>
+				</div>
+			</div>
+
+			 <div class="row">
+			 	<div class="container">
+			 		<div class="col-md-4">
+			 			<h4></h4>
+			 		</div>
+			 		<!-- `id`, `firstname`, `lastname`, `email`, `password` -->
+			 		<div class="col-md-4 well">
+			 			<form method="POST" action="">
+
+			 				<div class="form-group">
+			 					<label>Email</label>
+			 					<input type="email" class="form-control" name="email" placeholder="Enter Email">
+			 				</div>
+
+			 				<div class="form-group">
+			 					<label>Password</label>
+			 					<input type="password" class="form-control" name="password" placeholder="Enter Password">
+			 				</div>
+			 				<div class="form-group">
+			 					<button class="btn btn-primary" type="submit" name="login_btn">Login</button>
+			 				</div>
+			 				<p>Not yet a member ? <a href="register.php">Register</a></p>
+			 			</form>
+			 		</div>
+			 		<div class="col-md-4">
+			 			<h4></h4>
+			 		</div>
+			 	</div>
+			 </div>
+		</div>
+</body>
+</html>
